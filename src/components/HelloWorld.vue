8b5614c0010d75ed55b05c33837d22d76788e82e
Mick Dellaert <82022124+MickDellaert@users.noreply.github.com>
2021-07-22 21:19:19 +0200

    displaying recipe title and image in list

--- src/components/HelloWorld.vue
+++ src/components/HelloWorld.vue
@@ -1,25 +1,27 @@
 <template>
   <div class="hello">
     <img alt="food logo" src="@/assets/food-serving.png">
-
     <h1>{{ msg }}</h1>
-
-
   </div>
 
-  <div id="vue-app">
-    <h2>{{foodQuery}}</h2>
+  <div class="search">
+    <h2>{{ foodQuery }}</h2>
     <input v-model="foodQuery" placeholder="search by ingredient">
     <button @click="fetchFood">Search Food!</button>
   </div>
 
-  <div id="results">
-    <h2>{{mealTitle}}</h2>
-    <img v-bind:src="mealImg">
+  <div class="results">
+<!--    <h2>{{ mealResults }}</h2>-->
+    <ul>
+      <li v-for="meal in mealResults.meals" :key="meal.idMeal">
+        <h4>{{meal.strMeal}}</h4>
+        <img class="foodthumb" :src="meal.strMealThumb">
+      </li>
+    </ul>
+    <img class="foodthumb" v-bind:src="mealImg">
+
+
   </div>
-
-<!--  www.themealdb.com/api/json/v1/1/filter.php?i={{foodQuery}}-->
-
 
 </template>
 
@@ -27,26 +29,22 @@
 export default {
   name: 'HelloWorld',
 
-  data(){
-      return{
-        foodQuery: "",
-        msg: "Search for a recipe",
-        url: "www.themealdb.com/api/json/v1/1/filter.php?i=chicken_breast",
-        data: "",
-        mealTitle: "",
-        mealImg: ""
-      }
-    },
-    methods: {
+  data() {
+    return {
+      foodQuery: "",
+      msg: "Search for a recipe",
+      mealResults: "",
+    }
+  },
+  methods: {
 
-      async fetchFood(){
-        const response = await fetch( `https://www.themealdb.com/api/json/v1/1/filter.php?i=${this.foodQuery}`)
-        const results = await response.json();
-        console.log(results)
-        this.mealTitle = results.meals[2].strMeal;
-        console.log(this.mealImg = results.meals[2].strMealThumb)
-      }
-   }
+    async fetchFood() {
+      const response = await fetch(`https://www.themealdb.com/api/json/v1/1/filter.php?i=${this.foodQuery}`)
+      const results = await response.json();
+      this.mealResults = results
+      console.log(results)
+    }
+  }
 }
 
 
@@ -57,21 +55,32 @@ export default {
 h3 {
   margin: 40px 0 0;
 }
+
 ul {
   list-style-type: none;
   padding: 0;
 }
+
 li {
   display: inline-block;
   margin: 0 10px;
 }
+
 a {
   color: #42b983;
 }
 
-img{
+img {
   width: 100px;
-  fill: #ff4c00;
+}
+
+.foodthumb {
+  width: 200px;
+}
+
+.results{
+  display: flex;
+  justify-content: center;
 }
 
 </style>
